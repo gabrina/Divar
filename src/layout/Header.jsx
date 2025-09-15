3e001d901d6b5e3d947eee30b765c5c451e51411
Fatemeh Heidari <gabrina101@gmail.com>
2025-09-15 23:17:18 +0330

    logout functionality, done

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
 import Styles from "./Header.module.css";
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { clearCookie } from "../utils/cookie";
+import { toast } from "react-toastify";
 
 function Header() {
+  const [isShown, setIsShown] = useState(false);
+
+  const navigate = useNavigate();
+
+  const logoutHandler = () => {
+    clearCookie();
+    navigate("/", { replace: true });
+    toast.success("شما با موفقیت خارج شدید");
+  };
+
   return (
     <header className={Styles.header}>
       <div>
@@ -15,13 +28,27 @@ function Header() {
         </span>
       </div>
       <div>
-        <Link to="/auth">
-          <span>
+        <div className={Styles.menu} style={{ border: "1px solid red" }}>
+          <span
+            onClick={() => {
+              setIsShown((prev) => !prev);
+            }}
+          >
             <img src="profile.svg" />
             <p>دیوار من</p>
           </span>
+          <ul className={isShown ? "" : Styles.hidden}>
+            <li>
+              <Link to="/auth">آگهی های من</Link>
+            </li>
+            <li onClick={logoutHandler}>
+              خروج
+            </li>
+          </ul>
+        </div>
+        <Link to="/dashboard" className={Styles.button}>
+          ثبت آگهی
         </Link>
-        <Link to="/dashboard" className={Styles.button}>ثبت آگهی</Link>
       </div>
     </header>
   );
--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -11,10 +11,20 @@ const setCookie = (tokens) => {
 const getCookie = (cookieName) => {
   return document.cookie
     .split(";")
-    .find((token) => token.split("=")[0] === cookieName)?.split("=")[1];
+    .find((token) => token.split("=")[0] === cookieName)
+    ?.split("=")[1];
 };
 
 //?is used in case that if either of the tokens is non-existent,
 //  getCookie wouldn't return undefined
 
-export { setCookie, getCookie };
+const clearCookie = () => {
+  const cookies = document.cookie.split(";");
+  cookies.forEach((cookie) => {
+    const eqPos = cookie.indexOf("=");
+    const name = eqPos > -1 ? cookie.substr(0, eqPos).trim() : cookie.trim();
+    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+  });
+};
+
+export { setCookie, getCookie, clearCookie };
